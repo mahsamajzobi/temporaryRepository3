5036cc4caf4d56e51fe86917ebb9d6cc5411e86c
mahsa <mh.majzobi@gmail.com>
2026-02-11 21:19:34 +0330

    log in program updated

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-name = input ("give us ur name : ")
-print ("hello dear"+ name)
+username = input ("give us your name : ")
+password = input ("give us your password : ")
+if username == 'mahsa' and password=='123':
+    print ("hello dear"+ name)
+else :
+    print ("wrong username or password.")
+    
